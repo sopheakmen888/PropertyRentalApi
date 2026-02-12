2b21ae30e1a60b9e74ba9cb93dfa6b287f5bf35a
Your Name <your.email@example.com>
2026-02-12 21:17:59 +0700

    complete user device tracking

--- src/main/java/com/rental/PropertyRentalApi/Service/DeviceInfoService.java
+++ src/main/java/com/rental/PropertyRentalApi/Service/DeviceInfoService.java
@@ -1,10 +1,16 @@
 package com.rental.PropertyRentalApi.Service;
 
+<<<<<<< HEAD
 import org.springframework.stereotype.Service;
 import ua_parser.Client;
 import ua_parser.Parser;
 
 @Service
+=======
+import ua_parser.Client;
+import ua_parser.Parser;
+
+>>>>>>> 5c3f839 (complete user device tracking)
 public class DeviceInfoService {
 
     private final Parser parser = new Parser();
