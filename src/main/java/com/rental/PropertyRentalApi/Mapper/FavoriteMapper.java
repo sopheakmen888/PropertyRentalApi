b0f49800c4f7ddf2d3b3f697955be9f04b569c20
Your Name <your.email@example.com>
2026-02-25 21:20:42 +0700

    fix later

--- src/main/java/com/rental/PropertyRentalApi/Mapper/FavoriteMapper.java
+++ src/main/java/com/rental/PropertyRentalApi/Mapper/FavoriteMapper.java
@@ -2,6 +2,7 @@ package com.rental.PropertyRentalApi.Mapper;
 
 import com.rental.PropertyRentalApi.DTO.response.PropertyResponse;
 import com.rental.PropertyRentalApi.Entity.Favorites;
+import com.rental.PropertyRentalApi.Entity.UploadsImages;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
@@ -19,6 +20,15 @@ public interface FavoriteMapper {
         }
         return favorites.stream()
                 .map(this::toPropertyResponse)
+                .toList();
+    }
+
+    // ⭐ THIS FIXES YOUR ERROR
+    default List<String> map(List<UploadsImages> images) {
+        if (images == null) return List.of();
+
+        return images.stream()
+                .map(UploadsImages::getUrls)
                 .toList();
     }
 }
