08842dba6c24ba0bb7f5722a99154c11ab8911db
neathcmd <ksdf71464@gmail.com>
2026-02-27 07:32:50 +0700

    fix error commit history

--- src/main/java/com/rental/PropertyRentalApi/Repository/CategoryRepository.java
+++ src/main/java/com/rental/PropertyRentalApi/Repository/CategoryRepository.java
@@ -4,20 +4,13 @@ import com.rental.PropertyRentalApi.Entity.Categories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-<<<<<<< HEAD
 import java.util.Optional;
 
-=======
->>>>>>> e4594a1 (add image favorite)
 @Repository
 public interface CategoryRepository extends JpaRepository<Categories, Long> {
     // You can add custom query methods if needed
     // Example: find category by name
-<<<<<<< HEAD
      Optional<Categories> findByName(String name);
 
      boolean existsByName(String name);
-=======
-    // Optional<Categories> findByName(String name);
->>>>>>> e4594a1 (add image favorite)
 }
