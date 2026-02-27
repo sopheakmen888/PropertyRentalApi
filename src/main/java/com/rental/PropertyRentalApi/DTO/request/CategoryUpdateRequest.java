3bebadc3eb213299a5b213db8ada5d0f3673c647
neathcmd <ksdf71464@gmail.com>
2026-02-27 15:03:00 +0700

    complete for the day

--- src/main/java/com/rental/PropertyRentalApi/DTO/request/CategoryUpdateRequest.java
+++ src/main/java/com/rental/PropertyRentalApi/DTO/request/CategoryUpdateRequest.java
@@ -7,8 +7,4 @@ import jakarta.validation.constraints.NotBlank;
 public class CategoryUpdateRequest {
     @NotBlank
     private String name;
-<<<<<<< HEAD
-=======
-    private String description;
->>>>>>> e4594a1 (add image favorite)
 }
