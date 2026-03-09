3216d8c06b184429f7c33393e26b58687e33b27b
neathcmd <ksdf71464@gmail.com>
2026-03-09 16:22:21 +0700

    fix bug

--- src/main/java/com/rental/PropertyRentalApi/Service/Impl/ReviewServiceImpl.java
+++ src/main/java/com/rental/PropertyRentalApi/Service/Impl/ReviewServiceImpl.java
@@ -6,7 +6,6 @@ import com.rental.PropertyRentalApi.DTO.response.ReviewResponse;
 import com.rental.PropertyRentalApi.Entity.Properties;
 import com.rental.PropertyRentalApi.Entity.Reviews;
 import com.rental.PropertyRentalApi.Entity.Users;
-import com.rental.PropertyRentalApi.Mapper.MapperConfiguration;
 import com.rental.PropertyRentalApi.Mapper.ReviewMapper;
 import com.rental.PropertyRentalApi.Repository.PropertyRepository;
 import com.rental.PropertyRentalApi.Repository.ReviewRepository;
